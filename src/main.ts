98debca2e92722aed964c60e20a8f5786fa95ed7
Bernard Cesarz <cesarzb@protonmail.com>
2025-12-09 17:58:18 +0100

    Added Class

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,47 @@
+import dayjs from "dayjs";
+
 type Point = [number, Date];
 
-const today = new Date();
-const tomorrow = new Date();
-tomorrow.setDate(today.getDate() + 1);
-const data: Point[] = [
-  [1, today],
-  [2, tomorrow],
-];
+class WeeklyChart {
+  private container: HTMLElement | null;
+  private containerWidth: number = 0;
+  private containerHeight: number = 0;
+  private points: Point[] = [];
+
+  constructor(containerId: string) {
+    this.container = document.querySelector(`#${containerId}`);
+
+    if (this.container) {
+      this.containerWidth = this.container.clientWidth;
+      this.containerHeight = this.container.clientHeight;
+      const dayWidth = this.containerWidth / 7;
+    }
+  }
+
+  public drawPlot(): void {
+    this.points.forEach(() => {
+      let dataPoint = document.createElement("div");
+
+      dataPoint.className =
+        "absolute top-0 left-0 rounded outline-4 outline-stone-500";
+
+      if (this.container) {
+        this.container.appendChild(dataPoint);
+      }
+    });
+  }
+
+  private initializePoints() {
+    const startOfTheWeek = dayjs().startOf("week");
+    for (let i = 0; i < 7; i++) {
+      this.points.push([
+        100 * Math.random(),
+        startOfTheWeek.add(i, "day").toDate(),
+      ]);
+    }
+  }
+}
+
+const chart = new WeeklyChart("plot");
+
+chart.drawPlot();
